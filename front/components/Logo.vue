443b33642a60c48657781f74cc9e3529bc63841a
dellamowica <dellamowica@gmail.com>
2020-01-08 08:01:21 +0100

    first commit

---
+++ front/components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+<style type="text/css">
+.NuxtLogo {
+  animation: 1s appear;
+}
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
---
+++ front/pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        mapperfront
+      </h1>
+      <h2 class="subtitle">
+        An online squared based map editor
+      </h2>
+      <div>
+        {{ title }}
+      </div>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  async asyncData({ params }) {
+    const { data } = await axios.get('http://mapper.docker.localhost/api')
+
+    return {
+      title: data
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
